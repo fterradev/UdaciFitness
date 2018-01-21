540e2d02e2e345fd769740d407b1aa0214d698b6
Fernando Terra <nandobrt@gmail.com>
2018-01-21 15:03:54 -0200

    feat: Add UdaciSlider and UdaciSteppers

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import AddEntry from './components/AddEntry';
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+import { blue } from './utils/colors'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
+      <View style={[{backgroundColor: blue}]}>
         <AddEntry />
       </View>
-    );
+    )
   }
 }
