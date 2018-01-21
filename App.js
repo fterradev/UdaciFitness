01fb671106da2052f3d7befad0b627133b57fc4f
Fernando Terra <nandobrt@gmail.com>
2018-01-21 16:40:06 -0200

    feat: Change colors

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
-import { blue } from './utils/colors'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={[{backgroundColor: blue}]}>
+      <View>
         <AddEntry />
       </View>
     )
